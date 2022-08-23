3e1f9349c27703432c03568ee9cad4d2ae5ef69c
Saurabh884 <saurabh.vicky1995@gmail.com>
2022-08-23 11:58:41 +0530

    first commit

---
+++ monsterindia/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import React from "react"
+import Navbar from './Components/navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
