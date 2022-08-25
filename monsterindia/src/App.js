a1c3fe6b4d3c01037e0b605ef5b226bec8bb6b59
deepak7843 <deepakshankardubey123@gmail.com>
2022-08-25 21:41:47 +0530

    push

--- monsterindia/src/App.js
+++ monsterindia/src/App.js
@@ -1,14 +1,19 @@
 
-import './App.css';
+// import './App.css';
 import React from "react"
 import Navbar from './Components/Navbar/Navbar';
 import Footer from './Components/Footer/Footer';
+import Login from './Pages/Login';
+import MainRoutes from "./Pages/MainRoutes";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Footer/>
+      {/* <Login/> */}
+
+      <MainRoutes/>
     </div>
   );
 }
